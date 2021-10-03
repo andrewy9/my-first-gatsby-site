a1eebdebf02dab6d9f2c875e3e849b6fa68591ff
andrewy9 <andrew.yang993@gmail.com>
2021-10-03 14:59:05 +1300

    added layout components and applied to components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 const IndexPage = () => {
   return (
     <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby Site</h1>
-      <Link to="/about">About</Link>
-      <p>I'm making this by following the Gatsby Tutorial!</p>
+      <Layout pageTitle="Home Page">
+        <p>I'm making this by following the Gatsby Tutorial!</p>
+      </Layout>
     </main>
   )
 }
