fbdcd579e10cdc9f089d4ef6c23b4d8951edd27c
andrewy9 <andrew.yang993@gmail.com>
2021-10-03 14:35:50 +1300

    link features added

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ const AboutPage = () => {
     <main>
       <title>About Me</title>
       <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
       <p>Hi there! I'm a proud creator of this site, which I built with Gatsby.</p>
     </main>
   )
